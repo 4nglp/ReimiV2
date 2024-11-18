9f52b4575b261689ee302e529eaeb6a16546426d
4nglp <4nglpw@gmail.com>
2024-11-18 20:32:39 +0100

    removed tailwind css :sob:

--- src/renderer/pages/library/index.tsx
+++ src/renderer/pages/library/index.tsx
@@ -2,10 +2,11 @@ import EntryList from '../../components/EntryList';
 
 export default function Library() {
   return (
-    <>
-      <h1>Library</h1>
-      <h3>3asq</h3>
+    <div className="test-class">
+      <h1 className="text-3xl font-bold text-blue-500 mb-4">Library</h1>
+      <h3 className="text-xl text-gray-700">3asq</h3>
       <EntryList />
-    </>
+      <div className="bg-green-200 p-4 mt-4">Test Tailwind Working</div>
+    </div>
   );
 }
